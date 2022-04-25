a7326d43fa2ca7cc691e32f9a342034cf69a18fe
canbulgay <canbulgay@outlook.com>
2022-04-25 14:44:39 +0300

    Set app.blade.php a home page , add cdn links for mdi-images

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Epigra Assignment</title>
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
